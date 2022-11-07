35b8c94bf1cc7f16aeaa9de39377f47829006bd4
UsernameAnda <piccoaldi@gmail.com>
2022-11-08 02:43:58 +0700

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 """
 Mari belajar dulu !
+Ibu menyuruh anak beli kopi, jika ada rokok surya beli satu bungkus rokok.
 """
-print('Bapak')
-print('Ibu')
-print('Anak')
+print('Ibu:"Nak, Belikan Bapakmu kopi. jika ada rokok surya beli satu bungkus"')
+print('Anak: "Iya, Bu mana uang nya?')
+
+kopi_kapalapi = 10
+rokok_surya = 9
+
+print('anak berangkat ke toko')
+if kopi_kapalapi > 0 :
+    if rokok_surya == 0 :
+        print('Beli kopi')
+    else:
+        print('Beli kopi dan rokok surya satu bungkus')
+else:
+    print('Tidak jadi beli')
+print('anak pulang dan laporan sama ibuknya')
+
+
